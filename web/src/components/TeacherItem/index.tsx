2f6eb08dd203c346cce2614488cf078b6027e700
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-08-03 13:42:28 -0300

    Criando componente TeacherItem

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars1.githubusercontent.com/u/22474655?s=460&u=b14af544d098b4ada62f67feace43d51e7a7a1ca&v=4" alt="Alexandre Tanaka"/>
+        <div>
+          <strong>Alexandre Tanaka</strong>
+          <span>Quimica</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /><br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 20,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
